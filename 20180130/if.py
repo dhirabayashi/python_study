f68f7be4ed73268ce1954d1652a6aec9d5a8ccda
Daiki Hirabayashi <gigabowser@hotmail.co.jp>
2018-01-30 19:49:26 +0900

    Initial commit

---
+++ 20180130/if.py
@@ -0,0 +1,8 @@
+x = int(input('数字を入力してください'))
+
+if x < 0:
+    print('xは負の値です')
+elif x == 0:
+    print('xはゼロです')
+else:
+    print('xは正の値です')
---
+++ 20180130/range.py
@@ -0,0 +1,19 @@
+for i in range(5, 10):
+    print(i)
+
+print()
+
+for i in range(0, 10, 3):
+    print(i)
+
+print()
+
+for i in range(-10, -100, -30):
+    print(i)
+
+print()
+
+
+a = ['I', 'have', 'a', 'pen']
+for i in range(len(a)):
+    print(i, a[i])
